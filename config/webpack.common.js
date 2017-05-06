35fc7243273de400dff3ffc658b7979bde619734
Rasmus Jensen <rasmusjensen@Rasmuss-MacBook-Pro.local>
2017-05-06 19:34:05 +0200

    Init

---
+++ config/webpack.common.js
@@ -0,0 +1,52 @@
+var webpack = require("webpack");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        "polyfills": "./src/polyfills.ts",
+        "vendor": "./src/vendor.ts",
+        "app": "./src/main.ts"
+    },
+
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ["awesome-typescript-loader", "angular2-template-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader"
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["raw-loader", "sass-loader"]
+            }
+        ]
+    },
+
+    plugins: [
+        // Workaround for angular/angular#11580
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)@angular/,
+            "./src", // location of your src
+            {} // a map of your routes
+        ),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["app", "vendor", "polyfills"]
+        }),
+
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        }),
+        new CopyWebpackPlugin([
+            { from: "src/assets", to: "assets" }
+        ]),
+    ]
+};
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  //moduleId: module.id,
+  selector: "main-app",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent { }
