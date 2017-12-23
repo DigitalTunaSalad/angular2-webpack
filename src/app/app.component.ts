992a622dcade4f0cae81f7f061ab70d0bc44f8b0
Rasmus Sloth Jensen <rasmus@digitaltunaslad.com>
2017-12-23 14:09:21 +0100

    new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  //moduleId: module.id,
   selector: "main-app",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
+import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home";
-export const routes: Routes = 
-[
-    {
-        path:"",
-        redirectTo:"/home",
-        pathMatch: "full"
-    },
-    {
-        path: "home",
-        component: HomeComponent
-    }
-];
+import { AboutComponent } from "./about";
+export const routes: Routes =
+    [
+        {
+            path: "",
+            redirectTo: "/home",
+            pathMatch: "full"
+        },
+        {
+            path: "home",
+            component: HomeComponent
+        },
+        {
+            path: "about",
+            component: AboutComponent
+        }
+    ];
 
-export const routing = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
