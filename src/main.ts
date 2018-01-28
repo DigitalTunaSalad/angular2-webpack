8dab892726d17f55f50479b4f34b40fab967a367
Rasmus Sloth Jensen <rasmus@digitaltunaslad.com>
2018-01-28 13:44:46 +0100

    fixed reference

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./theming/app.scss";
+import "./theming/theme.scss";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app/app.module";
